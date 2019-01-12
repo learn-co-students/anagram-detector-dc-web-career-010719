33dcd9479ecee861aa08a35fdad6b6f62118a12b
canikwe <c.anikwe@gmail.com>
2019-01-12 14:30:07 -0500

    Anagram for anagram? 12 JAN 19

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+  	@word = word
+  end
+
+  def match(word_or_string)
+  	word_arr = self.word.split("").sort
+  	word_or_string.select do |word|
+  		word_arr == word.split("").sort
+  	end
+  end
+
+end
