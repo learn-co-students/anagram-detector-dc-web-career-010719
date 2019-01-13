b3f2a286fc822c8dcd64ac6957de2db803bd29c8
Heloise Taillet <heloise96@hotmail.com>
2019-01-13 13:01:38 -0500

    all tests pass

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    words.map do |word|
+      if word.split("").sort == anagram.split("").sort
+        word
+      end
+    end.uniq.reject {|word| word == nil}
+  end
+end
