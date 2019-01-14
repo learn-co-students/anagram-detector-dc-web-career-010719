7a4137d6a3034b430db1737e20584aab66273800
dcjones1 <dcejjones@gmail.com>
2019-01-14 13:32:30 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word, :anagrams
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    anagram_index = []
+    word_letters = self.word.split("").sort
+    anagrams_letters = anagrams.map {|word| word.split("").sort}
+    anagram_index = anagrams_letters.each_with_index.select {|word,index| word == word_letters}.map(&:last)
+    anagram_index.map {|num| anagrams[num]}
+  end
+end
