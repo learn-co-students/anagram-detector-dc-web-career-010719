428ac8f72158b2016f54d62b16e8a7796a565daa
dcjones1 <dcejjones@gmail.com>
2019-01-14 13:36:07 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,8 @@ class Anagram
   def match(anagrams)
     anagram_index = []
     word_letters = self.word.split("").sort
-    anagrams_letters = anagrams.map {|word| word.split("").sort}
-    anagram_index = anagrams_letters.each_with_index.select {|word,index| word == word_letters}.map(&:last)
+    #complex.. splits each word in anagrams into it's letters. then it compares that to the letters from the given word. it tracks the index of each set of anagram letters and if it finds a match, it maps the index value of the match. this gives us an array of indexes from the original array where we have an anagram.
+    anagram_index = anagrams.each_with_index.select {|word,index| word.split("").sort == word_letters}.map(&:last)
     anagram_index.map {|num| anagrams[num]}
   end
 end
