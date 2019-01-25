a14e15ebaa8829ce23180b61a97c07ae3b7356d8
James Clement <jameseclement@gmail.com>
2019-01-25 12:50:00 -0500

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(possible)
+  possible.select{|element|
+    element.split('').sort == word.split('').sort}
+
+  
+end
+
+end
